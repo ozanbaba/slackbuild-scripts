67fe691e03ab1a087019744e33fffe5fddf31be7
Ozan Türkyılmaz <ozan.turkyilmaz@gmail.com>
2011-12-13 21:39:46 +0500

    Fuck evolution. Use GSB version instead. It's well done.

--- evolution/evolution-exchange/doinst.sh
+++
@@ -1,4 +0,0 @@
-# Restart gconfd-2 if running to reload new gconf settings
-if ps acx | grep -q gconfd-2 ; then
-        killall -HUP gconfd-2 ;
-fi
--- evolution/heimdal-libraries/doinst.sh
+++
@@ -1,8 +0,0 @@
-# needed for API compatibility with MIT Krb5
-(cd usr/include/gssapi; ln -fs gssapi.h gssapi_generic.h);
-
-# Install new info files
-if [ -x usr/bin/install-info ]; then
-  usr/bin/install-info usr/info/hx509.info.gz usr/info/dir >/dev/null 2>&1
-  usr/bin/install-info usr/info/heimdal.info.gz usr/info/dir >/dev/null 2>&1
-fi
